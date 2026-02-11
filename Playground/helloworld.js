4c7d04b183b933dae925098b6f856bd8379b2149
Amik05 <amatianiu@gmail.com>
2026-02-10 20:12:06 -0800

    setting up typescript

---
+++ Playground/helloworld.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Ash";
+let friend = "Ketchup";
+// username = 25; // VS Code will immediately underline this in RED!
+console.log(`Hello, ${username} and my friend ${friend}`);
+//# sourceMappingURL=helloworld.js.map
---
+++ Playground/helloworld.ts
@@ -0,0 +1,4 @@
+let username: string = "Ash";
+let friend: string = "Ketchup";
+// username = 25; // VS Code will immediately underline this in RED!
+console.log(`Hello, ${username} and my friend ${friend}`);
